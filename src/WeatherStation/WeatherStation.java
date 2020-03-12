3b3d068bc5f1e2b0ae6bd45a1422118f7c9e74e0
ganjali <GAnjali@github.com>
2020-03-12 16:14:52 +0530

    Implement main class with functionality

---
+++ src/WeatherStation/WeatherStation.java
@@ -0,0 +1,22 @@
+package WeatherStation;
+
+import WeatherStation.Observers.CurrentConditionsDisplay;
+import WeatherStation.Observers.ForecastDisplay;
+import WeatherStation.Observers.StatisticsDisplay;
+import WeatherStation.Subject.WeatherData;
+
+public class WeatherStation {
+
+	public static void main(String[] args) {
+		WeatherData weatherData = new WeatherData();
+	
+		CurrentConditionsDisplay currentDisplay =
+			new CurrentConditionsDisplay(weatherData);
+		StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+		ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+
+		weatherData.setMeasurements(80, 65, 30.4f);
+		weatherData.setMeasurements(82, 70, 29.2f);
+		weatherData.setMeasurements(78, 90, 29.2f);
+	}
+}
